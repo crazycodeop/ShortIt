acea3678a6f7faf8d5a01b5055f8634a91606c12
Roshan Yadav <crazyopuser@outlook.com>
2023-11-08 17:11:51 +0530

    update

--- src/Link.js
+++ src/Link.js
@@ -6,35 +6,39 @@ const Link = ({inputValue}) => {
     const [shortenLink, setShortenLink]=useState("Hello World!");
     const [copied, setCopied]=useState(false);
     const [loading, setLoading]=useState(false);
-    const [data, setData] = useState([]);
-
-    const apiUrl = 'https://shrtlnk.dev/api/v2/link';
-
-    const headers = {
-      'api-key': 'swe9meqpwa80ntOFFmMa1zNF3OpplezNM5fYF9LmkWIGn',
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    };
-
-    const fetchData = (inputValue)=> {
-
-      const requestBody = {
-        url: inputValue,
+    const [error, setError]=useState(false);
+    
+    const fetchData = async (inputValue) => {
+      setLoading(true);
+      const options = {
+        method: 'POST',
+        url: 'https://shrtlnk.dev/api/v2/link',
+        headers: {
+          'Content-Type': 'application/json',
+          'api-key': 'swe9meqpwa80ntOFFmMa1zNF3OpplezNM5fYF9LmkWIGn',
+          'Accept': 'application/json'
+        },
+        data: {
+          url: inputValue
+        }
       };
-
-      axios.post(apiUrl, requestBody, { headers })
-        .then(response => {
-          setData(response.data);
-        })
+      
+      try {
+        const response = await axios.request(options );
+        console.log(response.data);
+        setShortenLink(response.data.shrtlnk);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
       }
+    }
 
     useEffect(()=>{
         if(inputValue.length) {
             fetchData(inputValue);
         }
     }, [inputValue])
-
-    console.log(data);
 
     useEffect(()=>{
         const timer=setTimeout(() => {
@@ -43,17 +47,33 @@ const Link = ({inputValue}) => {
         return ()=>clearTimeout(timer);
     }, [copied]);
 
+    if(loading) {
+      return <p className='noData'>
+        Loading...
+      </p>
+    }
+
+    if(error) {
+      return <p className='noData'>
+        Something went wrong ;(
+      </p>
+    }
+
   return (
-    <div className='result'>
-      {/* <p>{shortenLink}</p> */}
-      <CopyToClipboard 
-        text={shortenLink}
-        onCopy={()=>setCopied(true)}
-        >
-      <button className={copied?"copied":""}>Copy to clipboard</button>
-      </CopyToClipboard>
-    </div>
+    <>
+      {shortenLink && (
+        <div className='result'>
+        <p>{shortenLink}</p>
+        <CopyToClipboard 
+          text={shortenLink}
+          onCopy={()=>setCopied(true)}
+          >
+        <button className={copied?"copied":""}>Copy to clipboard</button>
+        </CopyToClipboard>
+      </div>
+      )}
+    </>
   )
 }
 
-export default Link
+export default Link;
